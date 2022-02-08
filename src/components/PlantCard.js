aa01a37f568166dfa0bf4afe2c06cd899534d160
Iuliia El Sammak <juliag875@gmail.com>
2022-02-08 10:17:47 -0500

    done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({id, name, image, price, onDeletePlant}) {
+  const[buyStatus, setBuyStatus] = useState(true)
+
+  function handleDeleteClick(){
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE"
+    })
+    .then(r=>r.json())
+    .then(()=>onDeletePlant({id}))
+  }
+
+  function changeBuyStatus(){
+    setBuyStatus(false)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {buyStatus ? ( <button onClick={changeBuyStatus} className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
+      <button style={{backgroundColor:"darkgreen", color:"white"}}
+      onClick={handleDeleteClick}
+      >Delete</button>
     </li>
   );
 }
