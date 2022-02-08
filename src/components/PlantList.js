1313aa7b8ba914d74659a5c0c6bf97780215aa52
Iuliia El Sammak <juliag875@gmail.com>
2022-02-08 00:42:38 -0500

    commit tree 38857de01f953b8455f1d5eafa469b5bc83cdf3b

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,19 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants, onDeletePlant}) {
+  const plantCards = plants.map(plant => 
+    <PlantCard 
+      key={plant.id}
+      id={plant.id}
+      name={plant.name} 
+      image={plant.image} 
+      price={plant.price}
+      onDeletePlant={onDeletePlant}
+    />
+    )
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantCards}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() =>{
+    fetch("http://localhost:6001/plants")
+    .then(r=>r.json())
+    .then(plants=>setPlants(plants))
+  }, [])
+
+  function addNewPlant(newItem){
+    setPlants([...plants, newItem])
+  }
+  
+  function handleDeletePlant(deletedPlant){
+    const updatedPlants = plants.filter(plant => plant.id !== deletedPlant.id);
+    setPlants(updatedPlants);
+  }
+  
+  const displayedPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()))
+
+  console.log(displayedPlants)
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={addNewPlant}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={displayedPlants } onDeletePlant={handleDeletePlant} />
     </main>
   );
 }
